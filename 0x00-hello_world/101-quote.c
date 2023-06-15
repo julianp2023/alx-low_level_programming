c176c6bd3d1e20713f025491d139606c88013025
julianp2023 <minaseamanuel07@gmail.com>
2023-06-15 12:32:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-    rite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return 1;
+    Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
      	
 }
