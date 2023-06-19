b4021ad4abbaef77fa715a4e4435f50278df345d
julianp2023 <minaseamanuel07@gmail.com>
2023-06-19 06:59:56 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,29 @@
  */
 int main(void)
 {
-    int d1, d2, d3;
+	int d1, d2, d3;
 
-    for (d1 = 0; d1 <= 7; d1++)
-    {
-        for (d2 = d1 + 1; d2 <= 8; d2++)
-        {
-            for (d3 = d2 + 1; d3 <= 9; d3++)
-            {
-                putchar(d1 + '0');
-                putchar(d2 + '0');
-                putchar(d3 + '0');
+	for (d1 = 0; d1 <= 7; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= 8; d2++)
+		{
+			for (d3 = d2 + 1; d3 <= 9; d3++)
+			{
+				putchar(d1 + '0');
+				putchar(d2 + '0');
+				putchar(d3 + '0');
 
-                if (d1 != 7 || d2 != 8 || d3 != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+				if (!(d1 == 7 && d2 == 8 && d3 == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
 
