573e46b46601e4fb76a67052b811b0823afbe755
julianp2023 <minaseamanuel07@gmail.com>
2023-06-19 06:10:32 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int tens_digit;
+    int ones_digit;
+
+    for (tens_digit = 0; tens_digit <= 8; tens_digit++)
+    {
+        for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+        {
+            putchar(tens_digit + '0');
+            putchar(ones_digit + '0');
+
+            if (tens_digit != 8 || ones_digit != 9)
+            {
+                write(1, ", ", 2);
+            }
+        }
+    }
+    write(1, "\n", 1);
+
+    return (0);
+}
+
