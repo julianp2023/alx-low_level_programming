1103cf39d0a50efd2132ad75ca9084ea1c04528e
julianp2023 <minaseamanuel07@gmail.com>
2023-06-19 02:15:34 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+
+	putchar('2');
+	putchar(',');
+	putchar(' ');
+
+	putchar('3');
+	putchar(',');
+	putchar(' ');
+
+	putchar('4');
+	putchar(',');
+	putchar(' ');
+
+	putchar('5');
+	putchar(',');
+	putchar(' ');
+
+	putchar('6');
+	putchar(',');
+	putchar(' ');
+
+	putchar('7');
+	putchar(',');
+	putchar(' ');
+
+	putchar('8');
+	putchar(',');
+	putchar(' ');
+
+	putchar('9');
+
+	putchar('\n');
+
+	return (0);
+}
+
