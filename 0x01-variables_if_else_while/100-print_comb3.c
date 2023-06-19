057f7299707dffb70cb62b20e16768d188ec4179
julianp2023 <minaseamanuel07@gmail.com>
2023-06-19 06:42:00 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,26 @@
  */
 int main(void)
 {
-    int firstDigit;
-    int secondDigit;
+	int firstDigit;
+	int secondDigit;
 
-    for (firstDigit = 0; firstDigit <= 8; firstDigit++)
-    {
-        for (secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
-        {
-            putchar(firstDigit + '0');
-            putchar(secondDigit + '0');
+	for (firstDigit = 0; firstDigit <= 8; firstDigit++)
+	{
+		for (secondDigit = firstDigit + 1; secondDigit <= 9; secondDigit++)
+		{
+			putchar(firstDigit + '0');
+			putchar(secondDigit + '0');
 
-            if (firstDigit != 8 || secondDigit != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (firstDigit != 8 || secondDigit != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
 
