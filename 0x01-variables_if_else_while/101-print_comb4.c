2c612ec6b825b9dffe7b8bd26bd1021e9d60044a
julianp2023 <minaseamanuel07@gmail.com>
2023-06-19 06:57:43 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,33 +3,27 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints all possible different combinations of four digits
+ * Description: Prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int d1, d2, d3, d4;
+    int d1, d2, d3;
 
-    for (d1 = 0; d1 <= 9; d1++)
+    for (d1 = 0; d1 <= 7; d1++)
     {
-        for (d2 = d1 + 1; d2 <= 9; d2++)
+        for (d2 = d1 + 1; d2 <= 8; d2++)
         {
             for (d3 = d2 + 1; d3 <= 9; d3++)
             {
-                for (d4 = d3 + 1; d4 <= 9; d4++)
+                putchar(d1 + '0');
+                putchar(d2 + '0');
+                putchar(d3 + '0');
+
+                if (d1 != 7 || d2 != 8 || d3 != 9)
                 {
-                    putchar(d1 + '0');
-                    putchar(d2 + '0');
                     putchar(',');
                     putchar(' ');
-                    putchar(d3 + '0');
-                    putchar(d4 + '0');
-
-                    if (!(d1 == 9 && d2 == 8 && d3 == 7 && d4 == 6))
-                    {
-                        putchar(',');
-                        putchar(' ');
-                    }
                 }
             }
         }
@@ -37,6 +31,6 @@ int main(void)
 
     putchar('\n');
 
-    return (0);
+    return 0;
 }
 
