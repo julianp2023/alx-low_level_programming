4fa88357b4040470d3d1c5b4e41e7dfa687b3d6f
julianp2023 <minaseamanuel07@gmail.com>
2023-06-20 13:45:20 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times in lowercase.
  */
 void print_alphabet_x10(void)
 {
-	int i;
 	char letter;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		letter = 'a';
-		while (letter <= 'z')
-		{
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-			letter++;
-		}
+
 		_putchar('\n');
 	}
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to the standard output
- * @c: the character to be written
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
  *
- * Return: On success, returns the character written.
- * On error, returns -1 and sets errno appropriately.
+ * Return: On success, the character written is returned.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+    return write(1, &c, 1);
 }
 
