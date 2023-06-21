87c21618889b3443223a2dc446d9a000ea230cac
julianp2023 <minaseamanuel07@gmail.com>
2023-06-21 13:55:14 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,27 +4,27 @@
  * main - Entry point
  *
  * Description: Prints the first 50 Fibonacci numbers
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-    int fib1 = 1; // First Fibonacci number
-    int fib2 = 2; // Second Fibonacci number
-    int fib3; // Variable to store the next Fibonacci number
-    int count; // Counter variable
+	int fib1 = 1; /* First Fibonacci number */
+	int fib2 = 2; /* Second Fibonacci number */
+	int fib3; /* Variable to store the next Fibonacci number */
+	int count; /* Counter variable */
 
-    printf("%d, %d", fib1, fib2); // Print the first two Fibonacci numbers
+	printf("%d, %d", fib1, fib2); /* Print the first two Fibonacci numbers */
 
-    for (count = 3; count <= 50; count++)
-    {
-        fib3 = fib1 + fib2;
-        printf(", %d", fib3);
-        fib1 = fib2;
-        fib2 = fib3;
-    }
+	for (count = 3; count <= 50; count++)
+	{
+		fib3 = fib1 + fib2;
+		printf(", %d", fib3);
+		fib1 = fib2;
+		fib2 = fib3;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return 0;
+	return 0;
 }
 
