ef2b5acfb52d5c7fbd2d2d447c51ea0e1bbc5c8d
julianp2023 <minaseamanuel07@gmail.com>
2023-06-21 10:46:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+int add(int a, int b);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
+
