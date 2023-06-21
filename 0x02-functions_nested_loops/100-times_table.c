dbdd14359016823add379618be46967816b227c7
julianp2023 <minaseamanuel07@gmail.com>
2023-06-21 13:06:02 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,40 @@
 #include "main.h"
 
-/**
- * print_times_table - Prints the n times table, starting with 0
- * @n: The number of times table to print
- */
 void print_times_table(int n)
 {
-	int i, j, product;
+    if (n < 0 || n > 15)
+        return;
 
-	if (n > 15 || n < 0)
-		return;
+    int i, j, product;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			product = i * j;
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            product = i * j;
+            if (j > 0)
+            {
+                if (product < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else if (product < 100)
+                {
+                    _putchar(' ');
+                }
+            }
+            _putchar(product / 100 % 10 + '0');
+            _putchar(product / 10 % 10 + '0');
+            _putchar(product % 10 + '0');
 
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (product < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (product < 100)
-				{
-					_putchar(' ');
-				}
-			}
-
-			if (product < 10 && j != 0)
-				_putchar(' ');
-
-			if (product >= 100)
-			{
-				_putchar((product / 100) + '0');
-				_putchar(((product / 10) % 10) + '0');
-			}
-			else if (product >= 10)
-			{
-				_putchar((product / 10) + '0');
-			}
-
-			_putchar((product % 10) + '0');
-		}
-		_putchar('\n');
-	}
+            if (j != n)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
 }
 
--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,13 +1,14 @@
-#include "100-times_table.c"
+#include "main.h"
 
 int main(void)
 {
-    print_times_table(2);
-    print_times_table(10);
-    print_times_table(13);
-    print_times_table(-2);
-    print_times_table(0);
-    print_times_table(100);
-    return (0);
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);
+    return 0;
 }
 
