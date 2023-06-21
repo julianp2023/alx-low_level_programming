e909f9454d66bb57ea23b8a2130f0398f24b714e
julianp2023 <minaseamanuel07@gmail.com>
2023-06-21 10:42:19 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/* Function prototype for add */
 int add(int a, int b);
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
