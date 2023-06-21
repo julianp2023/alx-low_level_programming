1a114f59619829a85f7fa91e477df200f2ed6981
julianp2023 <minaseamanuel07@gmail.com>
2023-06-21 05:43:14 -0700

    6-main.c

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int _abs(int num);
+
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    
+    return 0;
+}
+
