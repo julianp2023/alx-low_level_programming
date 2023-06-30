cc42d51e621438b24e91d9d159f5573194554cfc
julianp2023 <minaseamanuel07@gmail.com>
2023-06-30 04:48:40 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+/**
+ * rot13 - Encodes a string using ROT13.
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
