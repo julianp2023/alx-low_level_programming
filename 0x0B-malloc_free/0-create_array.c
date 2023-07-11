cec5cd03558ba970c9c1aee4e14ce7fd4133e3c3
julianp2023 <minaseamanuel07@gmail.com>
2023-07-10 23:53:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,17 +10,17 @@
 char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
-		return (NULL);
+		return NULL;
 
 	char *array = malloc(size * sizeof(char));
 	if (array == NULL)
-		return (NULL);
+		return NULL;
 
 	unsigned int i;
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
 
-	return (array);
+	return array;
 }
 
