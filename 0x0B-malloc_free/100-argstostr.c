06f8a770d82d3d1407386b10c4a12a5bd2ba7f27
julianp2023 <minaseamanuel07@gmail.com>
2023-07-11 01:05:18 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,41 +11,39 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int i, j, len, total_len = 0;
-    int newline_count;
+	char *str;
+	int i, j, len, total_len = 0;
+	int newline_count = ac - 1; /* Number of newlines needed */
 
-    if (ac == 0 || av == NULL)
-        return NULL;
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    // Calculate the total length of the concatenated string
-    for (i = 0; i < ac; i++)
-        total_len += strlen(av[i]);
+	/* Calculate the total length of the concatenated string */
+	for (i = 0; i < ac; i++)
+		total_len += strlen(av[i]);
 
-    newline_count = ac - 1;  // Number of newlines needed
+	/* Allocate memory for the concatenated string */
+	str = malloc(sizeof(char) * (total_len + newline_count + 1));
+	if (str == NULL)
+		return (NULL);
 
-    // Allocate memory for the concatenated string
-    str = malloc(sizeof(char) * (total_len + newline_count + 1));
-    if (str == NULL)
-        return NULL;
+	/* Copy each argument to the concatenated string */
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[len] = av[i][j];
+			len++;
+		}
+		if (i < ac - 1)
+		{
+			str[len] = '\n'; /* Add a newline after each argument */
+			len++;
+		}
+	}
+	str[len] = '\0'; /* Add null terminator at the end */
 
-    // Copy each argument to the concatenated string
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            str[len] = av[i][j];
-            len++;
-        }
-        if (i < ac - 1)
-        {
-            str[len] = '\n';  // Add a newline after each argument
-            len++;
-        }
-    }
-    str[len] = '\0';  // Add null terminator at the end
-
-    return str;
+	return (str);
 }
 
