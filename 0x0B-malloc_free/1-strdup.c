1e399bdb03ed59ecd91254f5ee85cc3cac856853
julianp2023 <minaseamanuel07@gmail.com>
2023-07-11 00:22:20 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,22 +8,23 @@
  */
 char *_strdup(char *str)
 {
-	if (str == NULL)
-		return (NULL);
-
 	unsigned int length = 0;
+	unsigned int i;
+	char *duplicate;
+
+	if (str == NULL)
+		return NULL;
+
 	while (str[length] != '\0')
 		length++;
 
-	char *duplicate = malloc((length + 1) * sizeof(char));
+	duplicate = malloc((length + 1) * sizeof(char));
 	if (duplicate == NULL)
-		return (NULL);
-
-	unsigned int i;
+		return NULL;
 
 	for (i = 0; i <= length; i++)
 		duplicate[i] = str[i];
 
-	return (duplicate);
+	return duplicate;
 }
 
