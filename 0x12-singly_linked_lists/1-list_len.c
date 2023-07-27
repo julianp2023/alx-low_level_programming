444e3653567dd56d04a1a526c4edffeaa7975a0c
julianp2023 <julianp2023>
2023-07-27 06:20:15 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,21 @@
-include "lists.h"
+#include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: Pointer to the head of the list.
  *
- * Return: number of elements in h
+ * Return: The number of elements in the list.
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		n++;
+		count++;
 		h = h->next;
 	}
-	return (n);
+
+	return (count);
 }
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+/* Structure for a linked list node */
 typedef struct list_s
 {
     char *str;
@@ -18,13 +11,11 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-/* Function prototypes */
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+/* Function to print the elements of a linked list */
 size_t print_list(const list_t *h);
-/* Add other function prototypes here if needed */
+
+/* Function to return the number of elements in a linked list */
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
