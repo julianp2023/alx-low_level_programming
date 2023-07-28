f011304809ae3e6320cf65fcd849a9304348f15c
julianp2023 <julianp2023>
2023-07-28 14:25:55 -0700

    update

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,24 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - check the code.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	int r;
+    int r;
 
-	r = print_sign(98);
-	printf("%d\n", r);
-
-	r = print_sign(0);
-	printf("%d\n", r);
-
-	r = print_sign(-98);
-	printf("%d\n", r);
-
-	return (0);
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
-
