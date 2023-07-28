6089ee71ccb7f90347413158e8e7762ba525793f
julianp2023 <julianp2023>
2023-07-28 13:19:06 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,48 @@
 #include "main.h"
-#include <ctype.h>
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * is_palindrome_recursive - Checks if a string is a palindrome using recursion
- * @s: The string to check
- * @start: The starting index of the current comparison
- * @end: The ending index of the current comparison
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
-int is_palindrome_recursive(char *s, int start, int end)
-{
-	char c1, c2;
-
-	if (start >= end)
-		return (1);
-
-	/* Skip non-alphabetic characters */
-	while (!isalpha(s[start]))
-		start++;
-	while (!isalpha(s[end]))
-		end--;
-
-	/* Convert characters to lowercase for case-insensitive comparison */
-	c1 = tolower(s[start]);
-	c2 = tolower(s[end]);
-
-	if (c1 != c2)
-		return (0);
-
-	return (is_palindrome_recursive(s, start + 1, end - 1));
-}
-
-/**
- * is_palindrome - Checks if a string is a palindrome
- * @s: The string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
-	int start = 0;
-	int end = 0;
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
 
-	while (s[len] != '\0')
-		len++;
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
 
-	end = len - 1;
-
-	return (is_palindrome_recursive(s, start, end));
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
 
